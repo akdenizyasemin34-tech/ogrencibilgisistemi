d6903172ace0537214570ae65692b3cea207654e
cagriakdeniz <akdenizyasemin34@gmail.com>
2025-12-30 23:07:02 +0300

    hata yakalama (try-catch) blokları eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,19 +9,31 @@ public class Main {
         System.out.println("--- ÖĞRENCİ KAYIT SİSTEMİ BAŞLATILIYOR ---");
         System.out.println("==========================================\n");
 
+        String adSoyad;
+        int id = 0;
 
-        System.out.print("Öğrencinin Adı Soyadı: ");
-        String adSoyad = scanner.nextLine();
 
-        System.out.print("Öğrenci ID: ");
-        int id = scanner.nextInt();
-        scanner.nextLine();
+        while (true) {
+            try {
+                System.out.print("Öğrencinin Adı Soyadı: ");
+                adSoyad = scanner.nextLine();
+
+                System.out.print("Öğrenci ID (Sadece Sayı): ");
+                id = scanner.nextInt();
+                scanner.nextLine();
+
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("HATA: Lütfen ID kısmına sadece sayı giriniz!");
+                scanner.nextLine();
+            }
+        }
 
         Student ogrenci = new Student(id, adSoyad);
         System.out.println("\nHoş geldin " + ogrenci.getName() + ". Giriş yapıldı.");
 
-
         boolean devamEtsinMi = true;
+
 
         while (devamEtsinMi) {
             System.out.println("\n-----------------------");
@@ -31,33 +44,52 @@ public class Main {
             System.out.println("0 - Sistemden Çıkış Yap");
             System.out.print("Seçiminiz: ");
 
-            int secim = scanner.nextInt();
-            scanner.nextLine();
+            int secim = -1;
+
+            try {
+                secim = scanner.nextInt();
+                scanner.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Lütfen menüdeki sayılardan birini girin.");
+                scanner.nextLine();
+                continue;
+            }
 
             if (secim == 1) {
 
-                System.out.print("Ders Kodu1: ");
+                System.out.print("Ders Kodu (Örn: CS101): ");
                 String kod = scanner.nextLine();
 
                 System.out.print("Ders Adı: ");
                 String dersAdi = scanner.nextLine();
 
-                System.out.print("Kredi: ");
-                int kredi = scanner.nextInt();
-                scanner.nextLine();
+                int kredi = 0;
+                while(true) {
+                    try {
+                        System.out.print("Kredi: ");
+                        kredi = scanner.nextInt();
+                        scanner.nextLine();
+                        break;
+                    } catch (InputMismatchException e) {
+                        System.out.println("HATA: Kredi sadece sayı olmalıdır!");
+                        scanner.nextLine();
+                    }
+                }
 
                 Course yeniDers = new Course(kod, dersAdi, kredi);
                 ogrenci.registerCourse(yeniDers);
 
             } else if (secim == 2) {
+
                 ogrenci.listCourses();
 
             } else if (secim == 0) {
+
                 System.out.println("Sistem kapatılıyor. İyi çalışmalar!");
                 devamEtsinMi = false;
 
             } else {
-                System.out.println("Hatalı bir seçim yaptınız! Lütfen tekrar deneyin.");
+                System.out.println("Geçersiz seçim! Tekrar deneyin.");
             }
         }
 
